414bc5abbcc0a58948f2955c6ce9ae9c21849b4d
rifatahammed <reardj007@gmail.com>
2021-09-02 15:31:45 +0600

    completed the assignment

--- js/app.js
+++ js/app.js
@@ -1,57 +1,56 @@
-// const loadBooks = () => {
-//   const searchText = document.getElementById("search-box").value;
-//   fetch(`http://openlibrary.org/search.json?q=${searchText}`)
-//     .then((response) => response.json())
-//     .then((data) => showBookName(data));
-//   // .then((data) => console.log(data));
-// };
-
-// const showBookName = (books) => {
-//   booksArray = books.docs;
-//   //   console.log(booksArray);
-//   booksArray.forEach((singlebook) => console.log(singlebook.title));
-// };
+document.getElementById("spinner").style.display = "none";
 document.getElementById("error-message").style.display = "none";
+document.getElementById("result-analysis").style.display = "none";
+document.getElementById("no-result").style.display = "none";
 const loadBooks = () => {
   const searchField = document.getElementById("search-field");
   const searchText = searchField.value;
   // clear data
   searchField.value = "";
+  document.getElementById("spinner").style.display = "block";
   document.getElementById("error-message").style.display = "none";
+  document.getElementById("result-analysis").style.display = "none";
   if (searchText == "") {
     // please write something to display
   } else {
     // load data
-    const url = `http://openlibrary.org/search.json?q=${searchText}`;
+    const url = `https://openlibrary.org/search.json?q=${searchText}`;
     fetch(url)
       .then((res) => res.json())
-      .then((data) => displaySearchResult(data));
+      .then((data) => displaySearchResult(data))
+      .catch((error) => displayError(error));
   }
 };
-// .catch((error) => displayError(error));
+
 const displayError = (error) => {
   document.getElementById("error-message").style.display = "block";
+  document.getElementById("spinner").style.display = "none";
 };
 
 const displaySearchResult = (allBooks) => {
-  const books = allBooks.docs;
+  const found = allBooks.numFound;
+  document.getElementById("books-found").innerText = found;
+  document.getElementById("spinner").style.display = "none";
+  document.getElementById("result-analysis").style.display = "block";
+  const books = allBooks.docs.slice(0, 20);
   const searchResult = document.getElementById("search-result");
   searchResult.textContent = "";
   if (books.length == 0) {
-    // show no result found;
+    document.getElementById("no-result").style.display = "block";
   }
+
   books.forEach((book) => {
-    // console.log(books);
     const div = document.createElement("div");
     div.classList.add("col");
     div.innerHTML = `
-        <div class="card h-100">
-        <h5 class="card-title">${book.title}</h5>
+        <div class="card h-100 rounded-3 border border-primary border-5">
+        <h5 class="card-title">Book Name:${book.title}</h5>
         <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="...">
             <div class="card-body">
             <h6>Author: ${book.author_name}</h6>
             <p class="card-text">
             publisher: ${book.publisher[0]}
+            <br/>
             first published: ${book.first_publish_year}</p>
             </div>
         </div>
@@ -59,26 +58,3 @@ const displaySearchResult = (allBooks) => {
     searchResult.appendChild(div);
   });
 };
-
-// const loadBookDetail = (mealId) => {
-//   const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
-//   fetch(url)
-//     .then((res) => res.json())
-//     .then((data) => displayMealDetail(data.meals[0]));
-// };
-
-// const displayMealDetail = (meal) => {
-//   console.log(meal);
-//   const mealDetails = document.getElementById("meal-details");
-//   const div = document.createElement("div");
-//   div.classList.add("card");
-//   div.innerHTML = `
-//     <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
-//     <div class="card-body">
-//         <h5 class="card-title">${meal.strMeal}</h5>
-//         <p class="card-text">${meal.strInstructions.slice(0, 150)}</p>
-//         <a href="${meal.strYoutube}" class="btn btn-primary">Go somewhere</a>
-//     </div>
-//     `;
-//   mealDetails.appendChild(div);
-// };
